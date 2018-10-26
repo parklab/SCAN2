7af98d9ff7eafff7a0cca35c3992e47a86c18beb
Luquette <ljl11@compute-e-16-236.o2.rc.hms.harvard.edu>
2018-10-25 22:12:49 -0400

    Refactor scripts for running SHAPEIT

--- scripts/run_shapeit.sh
+++ scripts/run_shapeit.sh
@@ -6,22 +6,23 @@
 #SBATCH --array=1-22
 #SBATCH --mem-per-cpu=8G
 
-if [ $# -lt 2 ]; then
-    echo "usage: ./phase_chr.sh vcf output_dir [chromosome]"
+if [ $# -lt 3 ]; then
+    echo "usage: ./phase_chr.sh vcf output_dir bulk_sample [chromosome]"
     exit 1
 fi
 
 vcf=$1
 output_dir=$2
+bulksample=$3
 
 if [ ! -z ${SLURM_ARRAY_TASK_ID+x} ]; then
     chr=$SLURM_ARRAY_TASK_ID
 else
-    if [ $# != 3 ]; then
+    if [ $# != 4 ]; then
         echo "Not in SLURM array; must specify chromosome"
         exit 1
     fi
-    chr=$3
+    chr=$4
 fi
 
 # SHAPEIT runs in 2 steps.  The first step prunes out SNPs in the
@@ -34,29 +35,49 @@ mkdir -p $OUTPUT_ROOT
 
 echo "Phasing chromosome=$chr from VCF=$vcf"
 
-chrvcf=$OUTPUT_ROOT/$(basename $vcf .vcf).chr$chr.vcf
-echo "-----------------"
-echo $chrvcf
+tmpfile=`mktemp`
 
-# Split by chromosome.  I think SHAPEIT provides an option to do this.
-awk "{ if (\$1 == \"$chr\" || \$0 ~ /^#/) print \$0; }" $vcf > $chrvcf
+cleanvcf=$OUTPUT_ROOT/in.vcf
+java -jar $GATK_PATH/GATK3.8.jar \
+    -R $GATK_PATH/human_g1k_v37_decoy.fasta \
+    -T SelectVariants \
+    -V $vcf \
+    -selectType SNP -restrictAllelesTo BIALLELIC -env -trimAlternates \
+    -select 'vc.getGenotype("'$bulksample'").isCalled()' \
+    -L $chr \
+    -o $cleanvcf
+
+echo $bulksample > $tmpfile
 
 $SHAPEIT_ROOT/bin/shapeit \
     -check \
-    --input-vcf=$chrvcf \
+    --input-vcf=$cleanvcf \
     -M $REFPANEL_ROOT/genetic_map_chr${chr}_combined_b37.txt \
     --input-ref $REFPANEL_ROOT/ALL.chr$chr.integrated_phase1_v3.20101123.snps_indels_svs.genotypes.nosing.haplotypes.gz \
         $REFPANEL_ROOT/ALL.chr$chr.integrated_phase1_v3.20101123.snps_indels_svs.genotypes.nosing.legend.gz \
         $REFPANEL_ROOT/ALL.integrated_phase1_v3.20101123.snps_indels_svs.genotypes.sample \
-    --include-ind samples_to_phase.txt \
+    --include-ind $tmpfile \
     --output-log $OUTPUT_ROOT/shapeit_check.log
 
 $SHAPEIT_ROOT/bin/shapeit \
-    --input-vcf=$chrvcf \
+    --input-vcf=$cleanvcf \
     -M $REFPANEL_ROOT/genetic_map_chr${chr}_combined_b37.txt \
     --input-ref $REFPANEL_ROOT/ALL.chr$chr.integrated_phase1_v3.20101123.snps_indels_svs.genotypes.nosing.haplotypes.gz \
         $REFPANEL_ROOT/ALL.chr$chr.integrated_phase1_v3.20101123.snps_indels_svs.genotypes.nosing.legend.gz \
         $REFPANEL_ROOT/ALL.integrated_phase1_v3.20101123.snps_indels_svs.genotypes.sample \
-    --include-ind samples_to_phase.txt \
+    --include-ind $tmpfile \
     --exclude-snp $OUTPUT_ROOT/shapeit_check.snp.strand.exclude \
     -O $OUTPUT_ROOT/1000g.cdt.nomulti.chr$chr.phased
+
+
+
+echo "Converting from SHAPEIT format to VCF."
+$SHAPEIT_ROOT/bin/shapeit -convert \
+    --input-haps $OUTPUT_ROOT/1000g.cdt.nomulti.chr$chr.phased \
+    --output-vcf $OUTPUT_ROOT/1000g.cdt.nomulti.chr$chr.phased.vcf
+
+echo "Filtering to het sites."
+awk '{ if ($10 == "1|0" || $10 == "0|1" || $1 ~ /^#/) print $0; }' \
+    $OUTPUT_ROOT/1000g.cdt.nomulti.chr$chr.phased.vcf \
+    | sed -e"s/$bulksample/phasedgt/g" > \
+      $output_dir/phased_hsnps.chr$chr.vcf
