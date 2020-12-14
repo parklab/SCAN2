10f9541f627d252a0d81dd69f6f3812a60d80bbd
Joe Luquette <lovelace_luquette@hms.harvard.edu>
2020-12-14 00:04:18 -0500

    Add final steps in object-based genotyper

--- snakemake/scripts/scansnv_cigar_scores_gather_script.R
+++ snakemake/scripts/scansnv_cigar_scores_gather_script.R
@@ -5,4 +5,8 @@ gt <- do.call(concat, lapply(snakemake@input[['rdas']], function(f) {
     get(load(f))
 }))
 
+gt <- add.static.filters(gt)
+gt <- compute.fdr.priors(gt)
+gt <- compute.fdr(gt)
+
 save(gt, file=snakemake@output[[1]])
