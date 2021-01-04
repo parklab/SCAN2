622e764bb00213aa1e4b3bdd5bdbc3f71ee77b74
Joe Luquette <lovelace_luquette@hms.harvard.edu>
2021-01-04 16:51:14 -0500

    Copy over sub-snakefiles on install

--- recipes/scan2/build.sh
+++ recipes/scan2/build.sh
@@ -3,5 +3,6 @@ cp bin/scan2 $PREFIX/bin
 mkdir -p $PREFIX/lib/scan2
 cp scripts/*.{R,sh,py} $PREFIX/lib/scan2/
 cp snakemake/Snakefile $PREFIX/lib/scan2/
+cp snakemake/snakefile.* $PREFIX/lib/scan2/
 mkdir -p $PREFIX/lib/scan2/scripts
 cp snakemake/scripts/*.R $PREFIX/lib/scan2/scripts
