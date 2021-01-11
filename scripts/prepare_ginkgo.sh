d8edd85ea40dfa837a2ab4f2a2529427f9d4b6de
Joe Luquette <lovelace_luquette@hms.harvard.edu>
2021-01-11 13:04:36 -0500

    Update to use new configuration file and bam entries

--- scripts/prepare_ginkgo.sh
+++ scripts/prepare_ginkgo.sh
@@ -8,7 +8,7 @@ fi
 ssdir=$1
 outdir=$2
 
-ssconfig=$ssdir/config.yaml
+ssconfig=$ssdir/scan.yaml
 
 
 scriptdir=$(cat $ssconfig | shyaml get-value scripts)
@@ -16,7 +16,7 @@ bamtobedscript=$scriptdir/run_bamtobed.sh
 ginkgoscript=$scriptdir/run_ginkgo.sh
 
 if [ ! -f $ssconfig ]; then
-    echo "ERROR: SCAN2 directory $ssdir does not contain a 'config.yaml'"
+    echo "ERROR: SCAN2 directory $ssdir does not contain a 'scan.yaml'"
     exit 1
 fi
 
@@ -24,7 +24,7 @@ echo "WARNING: be sure to 'conda activate ginkgo' before running this script!"
 
 mkdir -p $outdir 
 
-bams=$(cat $ssconfig | shyaml values bams)
+bams=$(cat $ssconfig | shyaml values bam_map)
 
 tmpdir=`pwd`
 cd $outdir
