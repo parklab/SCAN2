0bdc28c24fba5a6b54ffa87fdfe628e14d423565
Joe Luquette <jl@Joes-MacBook-Pro-3.local>
2022-05-17 18:46:30 -0400

    Add command line script interface to run.pipeline

---
+++ scripts/run_pipeline.R
@@ -0,0 +1,34 @@
+#!/usr/bin/env Rscript
+
+args <- commandArgs(trailingOnly=TRUE)
+if (length(args) != 10)
+    stop('usage: run_pipeline.R sc.sample bulk.sample mmq60.tab.gz mmq1.tab.gz hsnps.tab.gz abmodel_fits.rda single_cell_cigars.tab.gz bulk_cigars.tab.gz genome_string output.rda')
+
+
+sc.sample <- args[1]
+bulk.sample <- args[2]
+mmq60 <- args[3]
+mmq1 <- args[4]
+hsnps <- args[5]
+abmodel.fits <- args[6]
+sccigars <- args[7]
+bulkcigars <- args[8]
+genome.string <- args[9]
+out.rda <- args[10]
+
+if (file.exists(out.rda))
+    stop(paste('output file', out.rda, 'already exists, please delete it first'))
+
+
+library(scan2)
+library(future)
+plan(multicore)
+
+results <- run.pipeline(
+    sc.sample=sc.sample, bulk.sample=bulk.sample,
+    mmq60=mmq60, mmq1=mmq1,
+    hsnps=hsnps, abfits=abmodel.fits,
+    sccigars=sccigars, bulkcigars=bulkcigars,
+    genome=genome.string, verbose=TRUE)
+
+save(results, file=out.rda, compress=FALSE)
