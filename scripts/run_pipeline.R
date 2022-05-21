dd613339bfb7e10aa563c99a1661f04b7ac0bc6c
Joe Luquette <jl@Joes-MacBook-Pro-3.local>
2022-05-21 00:09:36 -0400

    Add support for CIGAR and FDR external data

--- scripts/run_pipeline.R
+++ scripts/run_pipeline.R
@@ -2,8 +2,8 @@
 
 args <- commandArgs(trailingOnly=TRUE)
 print(args)
-if (length(args) < 10)
-    stop('usage: run_pipeline.R sc.sample bulk.sample mmq60.tab.gz mmq1.tab.gz hsnps.tab.gz abmodel_fits.rda single_cell_cigars.tab.gz bulk_cigars.tab.gz genome_string output.rda [tmpsave.rda]')
+if (length(args) < 12)
+    stop('usage: run_pipeline.R sc.sample bulk.sample mmq60.tab.gz mmq1.tab.gz hsnps.tab.gz abmodel_fits.rda single_cell_cigars.tab.gz bulk_cigars.tab.gz cigar_training_data.rda fdr_prior_data.rda genome_string output.rda [tmpsave.rda]')
 
 sc.sample <- args[1]
 bulk.sample <- args[2]
@@ -13,8 +13,10 @@ hsnps <- args[5]
 abmodel.fits <- args[6]
 sccigars <- args[7]
 bulkcigars <- args[8]
-genome.string <- args[9]
-out.rda <- args[10]
+cigardata <- args[9]
+fdrpriordata <- args[10]
+genome.string <- args[11]
+out.rda <- args[12]
 
 if (file.exists(out.rda))
     stop(paste('output file', out.rda, 'already exists, please delete it first'))
@@ -25,25 +27,27 @@ library(future)
 library(progressr)
 plan(multicore)
 
-if (length(args) == 11) {
-    tmpsave.rda <- args[11]
-with_progress(
-    results <- run.pipeline(
-        sc.sample=sc.sample, bulk.sample=bulk.sample,
-        mmq60=mmq60, mmq1=mmq1,
-        hsnps=hsnps, abfits=abmodel.fits,
-        sccigars=sccigars, bulkcigars=bulkcigars,
-        tmpsave.rda=tmpsave.rda, genome=genome.string, verbose=TRUE)
-, enable=TRUE)
+if (length(args) == 13) {
+    tmpsave.rda <- args[13]
+    with_progress(
+        results <- run.pipeline(
+            sc.sample=sc.sample, bulk.sample=bulk.sample,
+            mmq60=mmq60, mmq1=mmq1,
+            hsnps=hsnps, abfits=abmodel.fits,
+            sccigars=sccigars, bulkcigars=bulkcigars,
+            trainingcigars=cigardata, fdr.prior.data=fdrpriordata,
+            tmpsave.rda=tmpsave.rda, genome=genome.string, verbose=TRUE)
+    , enable=TRUE)
 } else {
-with_progress(
-    results <- run.pipeline(
-        sc.sample=sc.sample, bulk.sample=bulk.sample,
-        mmq60=mmq60, mmq1=mmq1,
-        hsnps=hsnps, abfits=abmodel.fits,
-        sccigars=sccigars, bulkcigars=bulkcigars,
-        genome=genome.string, verbose=TRUE)
-, enable=TRUE)
+    with_progress(
+        results <- run.pipeline(
+            sc.sample=sc.sample, bulk.sample=bulk.sample,
+            mmq60=mmq60, mmq1=mmq1,
+            hsnps=hsnps, abfits=abmodel.fits,
+            sccigars=sccigars, bulkcigars=bulkcigars,
+            trainingcigars=cigardata, fdr.prior.data=fdrpriordata,
+            genome=genome.string, verbose=TRUE)
+    , enable=TRUE)
 }
 
 save(results, file=out.rda, compress=FALSE)
