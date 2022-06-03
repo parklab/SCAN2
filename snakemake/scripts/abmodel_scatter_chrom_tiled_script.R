fcd3829740a7a4d1443c9343d59d7e8ff6d29f09
Joe Luquette <jl@Joes-MacBook-Pro-3.local>
2022-06-03 04:35:18 -0400

    Update for integrated table

--- snakemake/scripts/abmodel_scatter_chrom_tiled_script.R
+++ snakemake/scripts/abmodel_scatter_chrom_tiled_script.R
@@ -10,7 +10,10 @@ if ('snakemake' %in% ls()) {
 
     commandArgs <- function(...) {
         ret <- unlist(c(
-            snakemake@input['hsnps'],
+            snakemake@input['inttab'],
+            snakemake@params['bulk_sample'],
+            snakemake@params['sc_sample'],
+            snakemake@params['genome'],
             snakemake@params['chrom'],
             snakemake@output['rda'],
             snakemake@params['n_tiles'],
@@ -23,19 +26,22 @@ if ('snakemake' %in% ls()) {
 }
 
 args <- commandArgs(trailingOnly=TRUE)
-if (length(args) < 4) {
+if (length(args) < 7) {
     cat('if n.cores is unspecified, default is 1\n')
-    stop("usage: abmodel_scatter_chrom_tiled_script.R hsnps.tab.bgz chromosome out.rda n.tiles [n.cores]")
+    stop("usage: abmodel_scatter_chrom_tiled_script.R integrated_table.tab.bgz bulk_sample_id sc_sample_id genome chromosome out.rda n.tiles [n.cores]")
 }
 
-hsnps.tab.bgz <- args[1]
-chrom <- args[2]
-out.rda <- args[3]
-n.tiles <- as.integer(args[4])
-if (length(args) == 5) {
+int.tab.bgz <- args[1]
+bulk.sample <- args[2]
+sc.sample <- args[3]
+genome <- args[4]
+chrom <- args[5]
+out.rda <- args[6]
+n.tiles <- as.integer(args[7])
+if (length(args) == 8) {
     library(future)
     plan(multicore)
-    n.cores <- as.integer(args[5])
+    n.cores <- as.integer(args[8])
 } else {
     plan(sequential)
     n.cores <- 1
@@ -51,9 +57,9 @@ suppressMessages(library(future))
 cat(future::availableCores(), "cores detected by library(future),", n.cores, "requested by user", "\n")
 plan(multicore, workers=n.cores)
 
-s <- make.scan(bulk='dummy.bulk', genome='hs37d5', single.cell='dummy.sc')
+s <- make.scan(bulk=bulk.sample, genome=genome, single.cell=sc.sample)
 
-f <- compute.ab.fits(s, path=hsnps.tab.bgz, chroms=chrom, n.tiles=n.tiles)
+f <- compute.ab.fits(s, path=int.tab.bgz, chroms=chrom, n.tiles=n.tiles)
 
 save(f, file=out.rda)
 
