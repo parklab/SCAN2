48efc74852abc4ff2c618b9756e935a023aa7e5b
Joe Luquette <jl@Joes-MacBook-Pro-3.local>
2022-06-03 17:26:25 -0400

    Update for integrated table

--- scripts/pregenotyping.R
+++ scripts/pregenotyping.R
@@ -1,23 +1,19 @@
 #!/usr/bin/env Rscript
 
 args <- commandArgs(trailingOnly=TRUE)
-if (length(args) != 10) {
+if (length(args) != 6) {
     cat("NOTE: do not specify the final .gz suffix for output files here; it will be created automatically. If '.gz' is included, it will be automatically removed.\n")
-    stop("usage: pregenotyping.R gatk.tab.gz gatk_lowmq.tab.gz hsnps.tab.gz sc_cigars.tab.gz bulk_cigars.tab.gz scan2_config.yaml out_hsnp_resampling.tab.gz out_hsnp_resampling_details.rda out_cigardata.tab.gz out.fdr.rda")
+    stop("usage: pregenotyping.R integrated_table.tab.gz sc_cigars.tab.gz bulk_cigars.tab.gz scan2_config.yaml out_cigardata.tab.gz out.fdr.rda")
 }
 
-gatk=args[1]
-gatklowmq=args[2]
-hsnps=args[3]
-sccigars=args[4]
-bulkcigars=args[5]
-scan2config=args[6]
-out.hsnps=sub('.gz$', '', args[7])
-out.hsnps.rda=args[8]
-out.cigars=sub('.gz$', '', args[9])
-out.fdr.rda=args[10]
+int.tab=args[1]
+sccigars=args[2]
+bulkcigars=args[3]
+scan2config=args[4]
+out.cigars=sub('.gz$', '', args[5])
+out.fdr.rda=args[6]
 
-for (outfile in c(paste0(out.hsnps, c('', '.gz', '.tbi')), paste0(out.cigars, c('', '.gz', '.tbi')), out.hsnps.rda, out.fdr.rda)) {
+for (outfile in c(paste0(out.cigars, c('', '.gz', '.tbi')), out.fdr.rda)) {
     if (file.exists(outfile))
         stop(paste0('output file ', outfile, ' already exists, please delete it first'))
 }
@@ -39,26 +35,16 @@ print(sc_id)
 # NOTE: the genome type in make.scan doesn't matter at all
 s <- make.scan(sc_id, bulk_id, 'hs37d5')
 s <- add.static.filter.params(s, scan2config)
-s <- read.gatk(s, gatk, quiet=TRUE, add.mutsig=FALSE)
-s <- read.gatk.lowmq(s, gatklowmq, quiet=TRUE)
-s <- add.training.data(s, hsnps, quiet=TRUE)
-s <- resample.training.data(s)
-resample.data <- s@resampled.training.data
-
-save(resample.data, file=out.hsnps.rda)
-out <- s@gatk[training.site==TRUE,.(chr, pos, refnt, altnt, training.hap1, training.hap2, training.dp, training.phgt, resampled.training.site)]
-colnames(out) <- c('#chr', 'pos', 'refnt', 'altnt', 'hap1', 'hap2', 'dp', 'phgt', 'resampled')
-fwrite(out, file=out.hsnps, sep='\t')
-Rsamtools::bgzip(out.hsnps, paste0(out.hsnps, '.gz'))
-system(paste('tabix -p vcf -S 1', paste0(out.hsnps, '.gz')))
+s <- read.integrated.table(s, int.tab, quiet=TRUE, add.mutsig=FALSE)
 
 s <- add.cigar.data(s, sccigars, bulkcigars, quiet=TRUE)
 null.sites <- cigar.get.null.sites(s, legacy=TRUE, quiet=TRUE)
 null.sites <- null.sites[, .(chr, pos, refnt, altnt, muttype, id.score.x, id.score.y, hs.score.x, hs.score.y)]
 colnames(null.sites)[1] <- "#chr"
 fwrite(null.sites, file=out.cigars, sep='\t')
-Rsamtools::bgzip(out.cigars, paste0(out.cigars, '.gz'))
-system(paste('tabix -p vcf -S 1', paste0(out.cigars, '.gz')))
+out.cigars.gz <- paste0(out.cigars, '.gz')
+Rsamtools::bgzip(out.cigars, out.cigars.gz)
+Rsamtools::indexTabix(file=out.cigars.gz, format='vcf', comment='#')
 
 s <- compute.fdr.prior.data(s)
 fdr.prior.data <- s@fdr.prior.data
