9c19aefdb840b22c8cf4140b1cfdc0a287812e99
Joe Luquette <jl@Joes-MacBook-Pro-3.local>
2022-06-08 07:22:22 -0400

    Several updates related to moving FDR prior data computation to call.mutations()

--- scripts/pregenotyping.R
+++ scripts/pregenotyping.R
@@ -1,9 +1,9 @@
 #!/usr/bin/env Rscript
 
 args <- commandArgs(trailingOnly=TRUE)
-if (length(args) != 8) {
+if (length(args) != 7) {
     cat("NOTE: do not specify the final .gz suffix for output files here; it will be created automatically. If '.gz' is included, it will be automatically removed.\n")
-    stop("usage: pregenotyping.R sc_sample_id bulk_sample_id integrated_table.tab.gz sc_cigars.tab.gz bulk_cigars.tab.gz scan2_config.yaml out_cigardata.tab.gz out.fdr.rda")
+    stop("usage: pregenotyping.R sc_sample_id bulk_sample_id integrated_table.tab.gz sc_cigars.tab.gz bulk_cigars.tab.gz genome out_cigardata.tab.gz")
 }
 
 sc.id=args[1]
@@ -11,29 +11,23 @@ bulk.id=args[2]
 int.tab=args[3]
 sccigars=args[4]
 bulkcigars=args[5]
-scan2config=args[6]
+genome=args[6]
 out.cigars=sub('.gz$', '', args[7])
-out.fdr.rda=args[8]
 
-for (outfile in c(paste0(out.cigars, c('', '.gz', '.tbi')), out.fdr.rda)) {
+for (outfile in c(paste0(out.cigars, c('', '.gz', '.tbi')))) {
     if (file.exists(outfile))
         stop(paste0('output file ', outfile, ' already exists, please delete it first'))
 }
 
 suppressMessages(library(scan2))
-suppressMessages(library(yaml))
 suppressMessages(library(Rsamtools))
-
-y <- yaml::read_yaml(scan2config)
 
 print(bulk.id)
 print(sc.id)
-print(y$genome)
+print(genome)
 
-s <- make.scan(sc.id, bulk.id, y$genome)
-s <- add.static.filter.params(s, scan2config)
+s <- make.scan(sc.id, bulk.id, genome)
 s <- read.integrated.table(s, int.tab, quiet=TRUE)
-
 s <- add.cigar.data(s, sccigars, bulkcigars, quiet=TRUE)
 null.sites <- cigar.get.null.sites(s, legacy=TRUE, quiet=TRUE)
 null.sites <- null.sites[, .(chr, pos, refnt, altnt, muttype, id.score.x, id.score.y, hs.score.x, hs.score.y)]
@@ -42,7 +36,3 @@ fwrite(null.sites, file=out.cigars, sep='\t')
 out.cigars.gz <- paste0(out.cigars, '.gz')
 Rsamtools::bgzip(out.cigars, out.cigars.gz)
 Rsamtools::indexTabix(file=out.cigars.gz, format='vcf', comment='#')
-
-s <- compute.fdr.prior.data(s)
-fdr.prior.data <- s@fdr.prior.data
-save(fdr.prior.data, file=out.fdr.rda)
--- scripts/run_pipeline.R
+++ scripts/run_pipeline.R
@@ -2,8 +2,8 @@
 
 args <- commandArgs(trailingOnly=TRUE)
 print(args)
-if (length(args) < 10)
-    stop('usage: run_pipeline.R sc.sample bulk.sample integrated_table.tab.gz abmodel_fits.rda single_cell_cigars.tab.gz bulk_cigars.tab.gz cigar_training_data.rda fdr_prior_data.rda genome_string output.rda [tmpsave.rda]')
+if (length(args) < 9)
+    stop('usage: run_pipeline.R sc.sample bulk.sample integrated_table.tab.gz abmodel_fits.rda single_cell_cigars.tab.gz bulk_cigars.tab.gz cigar_training_data.rda genome_string output.rda [tmpsave.rda]')
 
 sc.sample <- args[1]
 bulk.sample <- args[2]
@@ -12,9 +12,8 @@ abmodel.fits <- args[4]
 sccigars <- args[5]
 bulkcigars <- args[6]
 cigardata <- args[7]
-fdrpriordata <- args[8]
-genome.string <- args[9]
-out.rda <- args[10]
+genome.string <- args[8]
+out.rda <- args[9]
 
 if (file.exists(out.rda))
     stop(paste('output file', out.rda, 'already exists, please delete it first'))
@@ -25,8 +24,8 @@ library(future)
 library(progressr)
 plan(multicore)
 
-if (length(args) == 11) {
-    tmpsave.rda <- args[11]
+if (length(args) == 10) {
+    tmpsave.rda <- args[10]
     with_progress({
         handlers(handler_newline())
         results <- run.pipeline(
