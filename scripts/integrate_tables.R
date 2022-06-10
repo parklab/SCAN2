58c80bd06cd62e5ea292379eab1d55e292eee0c8
Joe Luquette <jl@Joes-MacBook-Pro-3.local>
2022-06-10 04:14:42 -0400

    Unlist to avoid NULL bug

--- scripts/integrate_tables.R
+++ scripts/integrate_tables.R
@@ -15,8 +15,8 @@ if ('snakemake' %in% ls()) {
             snakemake@input['phasing'],
             # if the user did not provide a panel, this entry is NULL and c() drops
             # NULL values silently. replace with NA and handle it later.
-            ifelse(is.null(snakemake@params['cross_sample_panel']), NA,
-                snakemake@params['cross_sample_panel']),
+            ifelse(is.null(snakemake@params[['cross_sample_panel']]), NA,
+                snakemake@params[['cross_sample_panel']]),
             snakemake@params['bulk_sample'],
             snakemake@params['genome'],
             snakemake@output['tab'],
