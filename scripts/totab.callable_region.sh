95d9a4d7dd644293a6f070b194330c2ef0adbf63
Joe Luquette <jl@Joes-MacBook-Pro-3.local>
2022-06-11 08:10:29 -0400

    Script for parsing GATK DepthOfCoverage joint table

---
+++ scripts/totab.callable_region.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+    echo "usage: $0 inputvcf output"
+    exit 1
+fi
+
+INPUT=$1
+OUTPUT=$2
+
+if [ -f $OUTPUT ] && [ $OUTPUT != '/dev/stdout' ]; then
+    echo "output file $OUTPUT already exists, please delete it first"
+    exit 1
+fi
+
+awk 'BEGIN { OFS="\t"; } {
+    if ($0 !~ /#/) {
+        split($1, firstcol, ":")
+        printf "%s\t%s\t%s\t%s\t%s\t%s", firstcol[1], firstcol[2];
+            for (i = 4; i <= NF; ++i) {
+            print "\t" $i;
+        }
+        print "\n";
+    } else {
+        if ($1 ~ /^Locus/) {
+            print "#chr\tpos";
+            for (i = 4; i <= NF; ++i) {
+                print "\t" sub(/^Depth_for_/, '', $i);
+            }
+            print "\n";
+        }
+    }
+}' $INPUT > $OUTPUT
