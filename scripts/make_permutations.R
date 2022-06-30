33fc9f7096667f289c4c4f145ba946a15827f48b
Joe Luquette <jl@Joes-MacBook-Pro-3.local>
2022-06-30 14:55:20 -0400

    Initial attempt at folding in old permutation scripts

---
+++ scripts/make_permutations.R
@@ -0,0 +1,84 @@
+#!/usr/bin/env Rscript
+
+# detect script being run by snakemake
+# if so, make a mock commandArgs function
+if ('snakemake' %in% ls()) {
+    logfile <- snakemake@log[[1]]
+    con <- file(logfile, 'w')
+    sink(con, type='output')
+    sink(con, type='message')
+
+    commandArgs <- function(...) {
+        ret <- unlist(c(
+            snakemake@input['muts'],
+            snakemake@params['sc_sample'],
+            snakemake@input['callable'],
+            snakemake@input['genome_file'],
+            snakemake@params['genome_string'],
+            snakemake@params['muttype'],
+            snakemake@params['passtype'],
+            snakemake@params['n_permutations'],
+            snakemake@output['rda'],
+            snakemake@threads
+        ))
+        ret
+    }
+    cat('Got command line arguments from snakemake:\n')
+    print(commandArgs())
+}
+
+args <- commandArgs(trailingOnly=TRUE)
+print(args)
+if (length(args) < 9) {
+    cat('mutations.csv may contain mutations from multiple single cells so long as a `sample` column is provided. It must also include columns: chr, pos, refnt, altnt, pass, rescue.\n')
+    cat("N.B.: genome_string must be a supported SCAN2 genome.\n")
+    stop("usage: make_permutations.R mutations.csv sc_sample callable_regions.bed genome_string bedtools_genomefile.txt {snv|indel} {pass|rescue} n_permutations out.rda [n.cores]")
+}
+
+muts.path <- args[1]
+sc.sample <- args[2]
+callable.path <- args[3]
+genome.string <- args[4]
+genomefile.path <- args[5]
+muttype <- args[6]
+passtype <- args[7]
+n.permutations <- as.integer(args[8])
+out.rda <- args[9]
+
+n.cores <- 1
+if (length(args) == 10)
+    n.cores <- as.integer(args[10])
+
+if (muttype != 'snv' & muttype != 'indel')
+    stop("muttype must be either 'snv' or 'indel' (case-sensitive)")
+
+if (passtype != 'pass' & passtype != 'rescue')
+    stop("passtype must be either 'pass' or 'rescue' (case-sensitive)")
+
+if (file.exists(out.rda))
+    stop(paste('output file', out.rda, 'already exists, please delete it first'))
+
+
+suppressMessages(library(scan2))
+suppressMessages(library(future))
+suppressMessages(library(progressr))
+plan(multicore, workers=n.cores)
+
+muts <- data.table::fread(muts.path)
+muts <- muts[sample == sc.sample & (pass == TRUE | (passtype == 'rescue' & rescue == TRUE))]
+
+progressr::with_progress({
+    # handler_newline causes alot of printing, but it's log-friendly
+    progressr::handlers(progressr::handler_newline())
+    results <- make.permuted.mutations(muts=muts,
+        sc.sample=sc.sample,
+        callable.bed=callable.path, genome=genome.string,
+        genome.file=genomefile.path, muttype=muttype,
+        n.permutations=n.permutations, quiet=TRUE)
+}, enable=TRUE)
+
+save(results, file=out.rda, compress=FALSE)
+
+if ('snakemake' %in% ls()) {
+    sink()
+}
