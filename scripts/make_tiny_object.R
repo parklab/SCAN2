b8858c1eb4cea9a7bc8d2d004f7a6c97ef473140
Joe Luquette <lovelace_luquette@hms.harvard.edu>
2023-06-04 21:12:23 -0400

    Remove large components of the spatial sensitivity models; convert models to summaries

--- scripts/make_tiny_object.R
+++ scripts/make_tiny_object.R
@@ -21,4 +21,20 @@ if ('rescue' %in% colnames(results@gatk)) {
     results@gatk <- results@gatk[pass == TRUE & somatic.candidate==TRUE]
 }
 
+# `data` contains 1kb-resolution bins tiling the whole genome. This table
+# is often ~1GB in size.
+results@spatial.sensitivity$data <- NULL
+
+# R's glm saves a copy of all data used to fit a model, so we keep only the model
+# summaries in the minimized object.  However, even the model summaries contain
+# a copy of the entire environment used for fitting in an attribute named
+#      attr(summary(model)$terms, '.Environment')
+# It appears that deleting this environment does not affect displaying basic info
+# about the model, such as fitted coefs and p-values.
+results@spatial.sensitivity$models <- lapply(results@spatial.sensitivity$models, function(m) {
+    s <- summary(m)
+    attr(s$terms, '.Environment') <- c()
+    s
+})
+
 save(results, file=out.rda)
