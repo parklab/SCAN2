16596a0422440999e869dc3d7372d91ccb7dc711
Joe Luquette <lovelace_luquette@hms.harvard.edu>
2024-04-05 20:53:35 -0400

    Calculate and save empirical CIGAR scores at null sites

--- scripts/pregenotyping.R
+++ scripts/pregenotyping.R
@@ -28,6 +28,23 @@ s <- read.integrated.table(s, int.tab, quiet=TRUE)
 s <- add.cigar.data(s, sccigars, bulkcigars, quiet=TRUE)
 null.sites <- cigar.get.null.sites(s, legacy=TRUE, quiet=TRUE)
 null.sites <- null.sites[, .(chr, pos, refnt, altnt, muttype, M.cigars, ID.cigars, HS.cigars, other.cigars, dp.cigars, M.cigars.bulk, ID.cigars.bulk, HS.cigars.bulk, other.cigars.bulk, dp.cigars.bulk)]
+
+# score the training sites. this is useful for two reasons:
+#   1. leave-one-out germline calling for sensitivity calibration.
+#   2. using a quantiles of the scores on training sites as the test cutoff.
+cat("Scoring null sites for CIGAR OPs..\n")
+compute.cigar.scores(null.sites)
+for (mt in c('snv', 'indel')) {
+    cat("muttype =", mt, '\n')
+    null.sites.mt <- null.sites[muttype == mt]
+
+    cat('I/D CIGAR ops\n')
+    id <- cigar.emp.score(training=null.sites.mt, test=null.sites.mt, which='id', quiet=FALSE, legacy=TRUE)
+    cat('H/S CIGAR ops\n')
+    hs <- cigar.emp.score(training=null.sites.mt, test=null.sites.mt, which='hs', quiet=FALSE, legacy=TRUE)
+
+    null.sites[muttype == mt, c('id.score', 'hs.score') := list(id, hs)]
+}
 colnames(null.sites)[1] <- "#chr"
 data.table::fwrite(null.sites, file=out.cigars, sep='\t')
 out.cigars.gz <- paste0(out.cigars, '.gz')
