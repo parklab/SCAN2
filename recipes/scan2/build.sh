b1664cef3dbd37aa4cd737b42ddc8d54bbea97c1
Joe Luquette <lovelace_luquette@hms.harvard.edu>
2024-04-05 20:48:34 -0400

    Attempt to inject the conda version number, build number and git hash string into the code on installation; copy resources files into install location

--- recipes/scan2/build.sh
+++ recipes/scan2/build.sh
@@ -2,9 +2,16 @@ mkdir -p $PREFIX/bin
 cp bin/scan2 $PREFIX/bin
 cp bin/digest_calls.R $PREFIX/bin
 cp bin/scan2_download_eagle_refpanel.sh $PREFIX/bin
+# Creates a file with nothing but a few version strings
+echo "version='$PKG_VERSION'" > bin/version.py
+echo "buildnum='$PKG_BUILDNUM'" >> bin/version.py
+echo "githash='$GIT_FULL_HASH'" >> bin/version.py
 mkdir -p $PREFIX/lib/scan2
 cp scripts/*.{R,sh,py} $PREFIX/lib/scan2/
 cp snakemake/Snakefile $PREFIX/lib/scan2/
 cp snakemake/snakefile.* $PREFIX/lib/scan2/
+mkdir -p $PREFIX/lib/scan2/resources
+cp -r resources/analysis_regions $PREFIX/lib/scan2/resources/
+cp -r resources/binned_counts $PREFIX/lib/scan2/resources/
 mkdir -p $PREFIX/lib/scan2/scripts
 cp snakemake/scripts/*.R $PREFIX/lib/scan2/scripts
